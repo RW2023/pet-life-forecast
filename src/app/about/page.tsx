1be8f9412685471ed4561068351bbb87458b42ee
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-19 09:59:35 -0500

    set about container hight to screen

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,25 +1,25 @@
-// Import necessary modules and components
 import { FC } from 'react';
-import Image from 'next/image'; // Ensure you use this if you plan to add images
 import Heading from '@/Components/Ui/Heading';
-import Link from 'next/link'; // Ensure you use this if you plan to add links
 
 interface Props {}
 
 const Page: FC<Props> = (props): JSX.Element => {
   return (
-    <div className="container mx-auto px-4">
-      <div className="w-full lg:w-4/5 mx-auto text-center py-12">
-        {/* Use the Heading component to display the title */}
-        <Heading title="Pet Life Forecast" />
+    // Use flex layout to ensure full height
+    <div className="flex flex-col min-h-screen">
+      <div className="container mx-auto px-4 flex-grow ">
+        <div className="w-full lg:w-4/5 mx-auto text-center py-12 ">
+          {/* Use the Heading component to display the title */}
+          <Heading title="Pet Life Forecast" />
 
-        {/* Description paragraph */}
-        <p>
-          Pet Life Forecast is a web application that allows pet owners to
-          determine the human years of their pets.
-        </p>
+          {/* Description paragraph */}
+          <p>
+            Pet Life Forecast is a web application that allows pet owners to
+            determine the human years of their pets.
+          </p>
 
-        {/* You can add Image or Link components here if needed */}
+          {/* You can add Image or Link components here if needed */}
+        </div>
       </div>
     </div>
   );
