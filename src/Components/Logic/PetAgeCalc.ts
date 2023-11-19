9e3a245dff16c8ec07b168b9773827a9db48b75e
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-19 11:34:43 -0500

    create age calculation function

--- src/Components/Logic/PetAgeCalc.ts
+++ src/Components/Logic/PetAgeCalc.ts
@@ -1,5 +1,5 @@
-function calculateCatAge() {
-  let catHumanAge = 1;
+// Function to calculate cat age in cat years
+function calculateCatAge(catHumanAge: number): number {
   let catAge = 0;
 
   if (catHumanAge === 0) {
@@ -11,7 +11,22 @@ function calculateCatAge() {
   } else {
     catAge = 24 + (catHumanAge - 2) * 4;
   }
-    return catAge;
+
+  return catAge;
 }
 
-export default calculateCatAge;
+// Function to calculate dog age in dog years
+function calculateDogAge(dogHumanAge: number): number {
+  let dogAge = 0;
+
+  if (dogHumanAge <= 2) {
+    dogAge = dogHumanAge * 10.5;
+  } else {
+    dogAge = 21 + (dogHumanAge - 2) * 4;
+  }
+
+  return dogAge;
+}
+
+// Exporting the functions
+export { calculateCatAge, calculateDogAge };
