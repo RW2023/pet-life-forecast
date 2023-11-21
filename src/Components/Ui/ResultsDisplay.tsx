0b734bb54b10857f4ee55434589134a92faec233
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-21 09:40:05 -0500

    conditionally render and angel or a reaper image based on a random number for now will add other functionality and conditions

--- src/Components/Ui/ResultsDisplay.tsx
+++ src/Components/Ui/ResultsDisplay.tsx
@@ -1,6 +1,7 @@
 // src/Components/Ui/ResultsDisplay.tsx
 import React from 'react';
-import Heading from '@/Components/Ui/Heading'; // Adjust the path as needed
+import Heading from '@/Components/Ui/Heading';
+import Image from 'next/image';
 
 interface ResultsDisplayProps {
   petType: 'cat' | 'dog';
@@ -15,15 +16,34 @@ const ResultsDisplay: React.FC<ResultsDisplayProps> = ({
 }) => {
   if (calculatedAge === null) return null;
 
+  // Randomly choose between rendering the reaper or the angel
+  const imageToShow = Math.random() < 0.5 ? '/reaper_bw.png' : '/angel.png';
+  const altText = imageToShow === '/reaper_bw.png' ? 'Reaper' : 'Angel';
+
   return (
-    <div className="results-container alert alert-success flex flex-col justify-between p-4">
+    <div className="results-container alert alert-success flex flex-col justify-between p-4 text-lg">
       <Heading title="Results" />
-      <p className="text-lg font-sans">
+
+      <h2>
+        {altText === 'Reaper' ? 'The Reaper Stalks' : 'An Angel Watches Over'}{' '}
+        your {petType}
+      </h2>
+      <Image
+        src={imageToShow}
+        alt={altText}
+        width={500}
+        height={300}
+        className="rounded-lg mt-0"
+      />
+
+      <p className="text-xl font-Poppins font-sans">
         Your {petType}&rsquo;s age in animal years is: {calculatedAge}
       </p>
-      <p className="text-lg font-sans">
-        The average life expectancy is: {lifeExpectancy} years
-      </p>
+      {lifeExpectancy && (
+        <p className="text-xl font-Poppins font-sans">
+          Life Expectancy: {lifeExpectancy} years
+        </p>
+      )}
     </div>
   );
 };
