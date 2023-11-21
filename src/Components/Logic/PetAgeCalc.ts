89b86024a00628da51e02dcf4bc73cc909c638ae
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-21 09:36:42 -0500

    update pet age calculator attempt to implement life expectancy Logic always returns 0 for life expectancy or some other low number calculation is increasing to fast will ts later

--- src/Components/Logic/PetAgeCalc.ts
+++ src/Components/Logic/PetAgeCalc.ts
@@ -1,50 +1,33 @@
-//src/Components/Logic/PetAgeCalc.ts
-'use client';
+// src/Components/Logic/PetAgeCalc.ts
 type BreedCategory = 'small' | 'medium' | 'large' | 'giant';
 
-// Function to calculate cat age in cat years
 function calculateCatAge(catHumanAge: number): number {
-  if (catHumanAge === 0) {
-    return 0;
-  } else if (catHumanAge === 1) {
-    return 15;
-  } else if (catHumanAge === 2) {
-    return 24;
-  } else {
-    return 24 + (catHumanAge - 2) * 4;
-  }
+  return catHumanAge === 0 ? 0 : 10 + (catHumanAge - 1) * 4;
 }
 
-// Function to calculate dog age in dog years
 function calculateDogAge(
   dogHumanAge: number,
-  breedCategory?: BreedCategory | undefined,
+  breedCategory: BreedCategory = 'medium',
 ): number {
   const dogLifespan = {
-    small: 12.5,
-    medium: 10.5,
-    large: 9,
-    giant: 8,
+    small: 4,
+    medium: 5,
+    large: 6,
+    giant: 7,
   };
 
-  const multiplier = breedCategory ? dogLifespan[breedCategory] : 10.5;
-
-  if (dogHumanAge <= 2) {
-    return dogHumanAge * multiplier;
+  if (dogHumanAge <= 1) {
+    return dogHumanAge * 10.5;
   } else {
-    return multiplier * 2 + (dogHumanAge - 2) * 4;
+    return 10.5 + (dogHumanAge - 1) * dogLifespan[breedCategory];
   }
 }
 
-// Function to get the average life expectancy based on species and breed
 function getAverageLifeExpectancy(
   species: string,
-  breedCategory?: BreedCategory | undefined,
+  breedCategory: BreedCategory = 'medium',
 ): number {
-  if (species === 'cat') {
-    return 15; // Average lifespan for cats
-  }
-
+  const catLifeExpectancy = 15;
   const dogLifespan = {
     small: 14,
     medium: 12,
@@ -52,25 +35,35 @@ function getAverageLifeExpectancy(
     giant: 8,
   };
 
-  return breedCategory ? dogLifespan[breedCategory] : 12; // Default to 12 if no breed category is provided
+  return species === 'cat' ? catLifeExpectancy : dogLifespan[breedCategory];
 }
 
-// Function to calculate life expectancy
 function lifeExpectancy(
   humanAge: number,
   species: string,
-  breedCategory?: BreedCategory | undefined,
+  breedCategory: BreedCategory = 'medium',
 ): number {
+  const animalAge =
+    species === 'cat'
+      ? calculateCatAge(humanAge)
+      : calculateDogAge(humanAge, breedCategory);
   const averageLifeExpectancy = getAverageLifeExpectancy(
     species,
     breedCategory,
   );
-  let animalAge =
-    species === 'cat'
-      ? calculateCatAge(humanAge)
-      : calculateDogAge(humanAge, breedCategory);
 
-  return Math.max(averageLifeExpectancy - animalAge, 0);
+  const remainingYearsAnimal = Math.max(averageLifeExpectancy - animalAge, 0);
+
+  const dogLifespanInverse = {
+    small: 1 / 4,
+    medium: 1 / 5,
+    large: 1 / 6,
+    giant: 1 / 7,
+  };
+
+  return species === 'cat'
+    ? remainingYearsAnimal / 4
+    : remainingYearsAnimal * dogLifespanInverse[breedCategory];
 }
 
 export { calculateCatAge, calculateDogAge, lifeExpectancy };
